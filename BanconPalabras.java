cbc6558d8d714ae88ea92ce3921d77b1a5a44d26
diego.bejarano@uabc.edu.mx <diego.bejarano@uabc.edu.mx>
2024-08-28 19:56:09 -0700

    primera version

---
+++ BanconPalabras.java
@@ -0,0 +1,11 @@
+class BanconPalabras{    
+    String palabra="banana";
+
+    public BanconPalabras(String palabra){
+        this.palabra = palabra;
+    }
+
+    public String damaUnaPalabra(){
+        return palabra;
+    }
+}
